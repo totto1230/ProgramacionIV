9198776599843f8c8e07cd8a8719dabaae6feffe
totto1230 <joseph.granados@hotmail.com>
2023-11-13 19:16:06 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_Joseph_Granados.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
